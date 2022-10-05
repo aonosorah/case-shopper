af0ddad8018dc09f47a2ebde8a6013b994774afc
aonosorah <102835599+aonosorah@users.noreply.github.com>
2022-10-05 16:20:05 -0300

    inicio backend, banco de dados criado e produtos adicionados ao sql, tipagens feitas

---
+++ backend/src/models/types.ts
@@ -0,0 +1,65 @@
+export interface IList {
+    id:string,
+    qty:number
+}
+
+export interface ICart {
+    id:string,
+    name:string,
+    date: Date,
+    products:IList[]
+}
+
+export class Cart {
+    constructor (
+        private id:string,
+        private name:string,
+        private date:Date,
+        private products:IList[]
+    ) {}
+        public getId = () => {
+            return this.id
+        }
+        public getName = () => {
+            return this.name
+        }
+        public getDate = () => {
+            return this.date
+        }
+        public getProducts = () => {
+            return this.products
+        }
+
+        public setId = (newId:string) => {
+            this.id = newId
+        }
+        public setName = (newName:string) => {
+            this.name = newName
+        }
+        public setDate = (newDate:Date) => {
+            this.date = newDate
+        }
+        public setProduct = (newProduct:IList[]) => {
+            this.products = newProduct
+        }
+
+}
+
+export class Products {
+    constructor (
+        private id:string,
+        private qty:number
+    ) {}
+        public getId = () => {
+            return this.id
+        }
+        public getQty = () => {
+            return this.qty
+        }
+        public setId = (newId:string) => {
+            this.id = newId
+        }
+        public setQty = (newQty:number) => {
+            this.qty = newQty
+        }
+}
