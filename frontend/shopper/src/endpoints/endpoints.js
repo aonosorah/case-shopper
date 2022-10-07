fe1775a6f059ea38de6478a101bc840c1d69de68
aonosorah <102835599+aonosorah@users.noreply.github.com>
2022-10-07 13:46:26 -0300

    função axios feita, aguardando testes

---
+++ frontend/shopper/src/endpoints/endpoints.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const base_URL = "https://shopper-case.herokuapp.com/"
+
+export const GetProducts = async () => {
+    try {
+        const res = await axios
+        .get(`${base_URL}/products`)
+        return res
+    } catch (err) { alert("Por favor, tente novamente" )}
+}
+
+export const CreateOrder = async (body) => {
+    try {
+         await axios
+        .post(`${base_URL}/create/order`, body)
+    } catch (err) { alert( "Algum dado pode estar faltando, por favor, tente novamente" )}
+}
